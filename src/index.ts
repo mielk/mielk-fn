b12643b051a7c39172bae3725acd795b86cd7054
mielk <mielk@o2.pl>
2024-10-13 12:47:18 +0200

    Regex added as global.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import arrays from './methods/arrays.js';
 import objects from './methods/objects.js';
+import regex from './methods/regex.js';
 import strings from './methods/strings.js';
 import variables from './methods/variables.js';
 
-export { arrays, objects, strings, variables };
+export { arrays, objects, regex, strings, variables };
 
-export default { arrays, objects, strings, variables };
+export default { arrays, objects, regex, strings, variables };
--- src/methods/regex.ts
+++ src/methods/regex.ts
@@ -1,8 +1,6 @@
 const getCapturedNumber = (input: string, pattern: string): number | undefined => {
 	const match = input.match(pattern);
-	console.log(match);
 	if (match && match[1]) {
-		console.log(match);
 		const num: number = parseInt(match[1], 10);
 		return num;
 	}
